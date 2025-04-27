0b3b3344abad87d24e45b664b4bd2e8ce87c4be0
Sam Ishola <Sam.Ishola@ibm.com>
2025-04-27 21:55:53 +0100

    new: Added Solution for LeetCode 653 Two Sum IV - Input is a BST

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ Two Sum II✅ Using Pointers
 3Sum ✅ Using Sorting and Two Pointers
 4Sum✅ Using Pointers
 
+### Trees
+
+Two Sum IV ✅ BST, sorted using inorder traversal
+
 ### Linked List
---
+++ Trees/TwoSumIV.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=653 lang=java
+ *
+ * [653] Two Sum IV - Input is a BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        //Create an array list to store the values of the BST
+        List<Integer> nums = new ArrayList<>();
+        //Sort the BST using inorder traversal which is a recursive DFS method that returns BSTs in ascending order
+        inorder(root, nums);
+        //Create Two Pointers to find the value pairs that add to k
+        int left = 0;
+        int right = nums.size()-1;
+        //Iterate through values in array List
+        while(left < right){
+            if(nums.get(left) + nums.get(right) == k) return true;
+            else if(nums.get(left) + nums.get(right) < k) left++;
+            else{
+                right--;
+            }
+        }
+        return false;
+    }
+    //Inorder traversal method
+    public void inorder(TreeNode root, List<Integer> nums){
+        //base case if the root num doesnt exist return nothing
+        if(root == null) return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+}
+// @lc code=end
